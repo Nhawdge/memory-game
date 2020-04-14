842587ae3d7d846cf26f8bd1877881475261dd2d
John Pavek <JohnQPavek@gmail.com>
2020-04-13 22:00:47 -0500

    Initial commit

---
+++ memory.js
@@ -0,0 +1,67 @@
+document.getElementById("sets").addEventListener("input", (x, ev) => {
+    document.getElementById("count").innerText = x.target.value;
+})
+
+document.querySelector('[data-game="start"]').addEventListener("click", StartGame)
+
+function StartGame() {
+
+    var numberOfSets = parseInt(document.getElementById("sets").value);
+    var setlist = new Array(numberOfSets + 1).join("x").split("").map(x => MakeSet());
+    setlist = RandomizeList([...setlist, ...setlist]);
+
+    var game = document.getElementById("game");
+    game.innerText = "";
+    for (card of setlist) {
+        var tile = document.createElement("div");
+        tile.classList.add("hidden");
+        tile.innerText = card;
+        tile.addEventListener("click", CheckCard)
+        game.appendChild(tile)
+    }
+
+
+}
+var canMove = true;
+var score = 0;
+
+function CheckCard(event) {
+    if (!canMove) {
+        return;
+    }
+    var cardsVisible = document.querySelectorAll("#game>div:not(.hidden)")
+    var card = event.target;
+    console.log(cardsVisible);
+    if (cardsVisible.length >= 1) {
+        canMove = false;
+        card.classList.toggle("hidden");
+
+
+
+        setTimeout(() => {
+            cardsVisible.forEach(x => x.classList.toggle("hidden"));
+            card.classList.toggle("hidden");
+            if (cardsVisible[0].innerText == card.innerText) {
+                card.remove();
+                cardsVisible[0].remove();
+                score += 1;
+            }
+            canMove = true;
+        }, 2000);
+    } else {
+        card.classList.toggle("hidden");
+
+    }
+}
+
+function MakeSet() {
+    return String.fromCharCode((Math.floor(Math.random() * 2000) % 2000) + 9000);
+}
+
+function RandomizeList(list) {
+    var outList = [];
+    while (list.length) {
+        outList.push(list.splice(Math.floor(Math.random() * list.length), 1)[0]);
+    }
+    return outList;
+}
